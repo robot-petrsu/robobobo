ac0e3d9c4083175203daf6ef8e2dcf4e58e6a692
Robot <sstepa1@rambler.ru>
2015-12-21 14:02:25 +0300

    Код для дальномера HC-SR04

---
+++ mraa-hc-sr04/src/mraa-hc-sr04.cpp
@@ -0,0 +1,69 @@
+/*
+ * Author: Yevgeniy Kiveisha <yevgeniy.kiveisha@intel.com>
+ * Copyright (c) 2014 Intel Corporation.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include <unistd.h>
+#include <iostream>
+#include "hcsr04.h"
+#include <signal.h>
+#include <stdlib.h>
+#include <sys/time.h>
+
+upm::HCSR04 *sonar1 = NULL;
+upm::HCSR04 *sonar2 = NULL;
+
+void
+sig_handler(int signo)
+{
+    printf("got signal\n");
+    if (signo == SIGINT) {
+        printf("exiting application\n");
+        sonar1->m_doWork = 1;
+        sonar2->m_doWork = 1;
+    }
+}
+
+//! [Interesting]
+int
+main(int argc, char **argv)
+{
+    sonar1 = new upm::HCSR04(3, 2);
+    sonar2 = new upm::HCSR04(5, 4);
+
+    signal(SIGINT, sig_handler);
+
+    sleep(1);
+
+    for(;;){
+        double distance = sonar1->getDistance(CM);
+        std::cout << "distance 1 = " << distance << std::endl;
+
+        //distance = sonar2->getDistance(CM);
+		//std::cout << "distance 2 = " << distance << std::endl;
+		sleep(1);
+    }
+
+    delete sonar1;
+    delete sonar2;
+    return 0;
+}
